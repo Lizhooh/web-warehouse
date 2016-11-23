84b2f0c7cf64a9cf2e1dec31bb5e62912012af13
lizhooh <960316408@qq.com>
2016-11-23 23:23:31 +0800

    set

--- Mychat/web/js/reset.js
+++
@@ -1,9 +0,0 @@
-
-    // 重命名 Electron 提供的 require
-    window._require = window.require;
-    window._exports = window.exports;
-    window._module  = window.module;
-
-    delete window.require;
-    delete window.exports;
-    delete window.module;
--- README.md
+++ README.md
@@ -11,4 +11,3 @@ web-warehouse | web 项目仓库 | private
 ##### imQQ: 仿 I'm QQ
 ##### NavShrink: jquery 导航收缩插件
 ##### login: 一个CSS3动画登录页面
-##### Mychat: 基于socket.io的即时聊天应用
